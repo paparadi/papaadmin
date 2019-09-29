32770f3f4076efa7f4eaa428a1c374c8b81cdc55
eslamradi <eslamradii@gmail.com>
2019-09-30 01:36:54 +0200

    update authentication routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,8 +2,12 @@
 	
 	Route::group(['namespace' => 'Paparadi\Papaadmin\Http\Controllers', 'middleware' => ['web']], function(){
 		Route::prefix('/admin')->name('admin.')->namespace('Admin')->group(function(){
-			
+			Route::namespace('Auth')->group(function(){
+				
+				Route::get('/login','LoginController@show')->name('login');
+				Route::post('/login','LoginController@login');
+				Route::post('/logout','LoginController@logout')->name('logout');	
+			});
 		});
-
 	});
 ?>
