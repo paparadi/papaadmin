b033b6e7f59d19a5599724c3816e35ca47a3c765
eslamradi <eslamradii@gmail.com>
2019-09-30 01:08:23 +0200

    initialize package servie provider

---
+++ src/PapaAdminServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Paparadi\Papaadmin;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Arr;
+
+
+class PapaAdminServiceProvider extends ServiceProvider {
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        
+    }
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+       
+
+    }
+}
+?>
