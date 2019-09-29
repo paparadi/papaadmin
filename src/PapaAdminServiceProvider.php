80386df423334ed7d99d24f9c086f9ca45a69aee
eslamradi <eslamradii@gmail.com>
2019-09-30 01:20:18 +0200

    update package ServiceProvider to load routes and merge config file with laravels auth config file

--- src/PapaAdminServiceProvider.php
+++ src/PapaAdminServiceProvider.php
@@ -7,24 +7,67 @@ use Illuminate\Support\Arr;
 
 
 class PapaAdminServiceProvider extends ServiceProvider {
-    /**
-     * Bootstrap services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        
-    }
-    /**
-     * Register services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-       
+	/**
+	 * Bootstrap services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->loadRoutesFrom(__DIR__.'/routes/web.php');
+	}
+	/**
+	 * Register services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->mergeConfigFromm(
+			__DIR__.'/config/admin.php', 'auth'
+		);
+	}
+	 /**
+	 * Merge the given configuration with the existing configuration.
+	 *
+	 * @param  string  $path
+	 * @param  string  $key
+	 * @return void
+	 */
+	protected function mergeConfigFromm($path, $key)
+	{
+		$config = $this->app['config']->get($key, []);
 
-    }
+		$this->app['config']->set($key, $this->mergeConfigs(require $path, $config));
+	}
+
+	/**
+	 * Merges the configs together and takes multi-dimensional arrays into account.
+	 *
+	 * @param  array  $original
+	 * @param  array  $merging
+	 * @return array
+	 */
+	protected function mergeConfigs(array $original, array $merging)
+	{
+		$array = array_merge($merging, $original);
+
+		foreach ($original as $key => $value) {
+			if (! is_array($value)) {
+				continue;
+			}
+
+			if (! Arr::exists($merging, $key)) {
+				continue;
+			}
+
+			if (is_numeric($key)) {
+				continue;
+			}
+
+			$array[$key] = $this->mergeConfigs($value, $merging[$key]);
+		}
+		return $array;
+	}
 }
 ?>
